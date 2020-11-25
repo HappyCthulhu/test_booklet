453562b8d4acb6434d8aa8936ba24a15d75de8f7
HappyCthulhu <valeriyg98@gmail.com>
2020-11-25 03:59:21 +0300

    add test_ask_order_exist

--- test.py
+++ test.py
@@ -14,3 +14,10 @@ class TestsForOrderBook:
     def test_if_order_is_not_exist(self, book):
         assert book.get_order(4545) == None, 'Existing order with incorrect ID'
     
+    def test_ask_order_exist(self, book):
+        price = 1
+        quantity = 4
+        type = 'ask'
+        order_ID = book.add_order(price, quantity, type)
+        assert book.get_order(order_ID), 'There is no ask-order'
+
